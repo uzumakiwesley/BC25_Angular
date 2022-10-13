be108452bb61cb2a238752442de8c79452784441
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-13 05:42:58 -0300

    primeiro commit angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+/**
+ *  'templateUrl' serve para informar ao componente qual arquivo HTML
+ * ele deve mostrar quando o mesmo for utilizado
+ * '
+ * 'styleUrls' serve para informar os caminhos dos arquivos CSS que o 
+ * componente utilizará
+ * 
+ * 'selector' serve para informar como aquele componente poderá 
+ * ser chamado dentro do projeto Angular dentro de outros arquivos
+ * HTML de outros componentes
+ *  
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
