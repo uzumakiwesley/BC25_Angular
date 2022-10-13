b018fcc118cbc49683d95b8edb98f66d476da5fc
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-13 18:06:24 -0300

    commit card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
+  /**
+   * declarations serve para registrar componentes, pipes, diretivas e outras estrututas
+   */
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    NavbarComponent
   ],
+  /**
+   * imports serve para módulos dentro de outros módulos
+   * 
+   * SERVE EXCLUSIVAMENTE PARA OUTROS MÓDULOS
+   */
   imports: [
     BrowserModule
   ],
+  /**
+   * providers serve para registrae serviços e interceptadores HTTP
+   */
   providers: [],
   bootstrap: [AppComponent]
 })
